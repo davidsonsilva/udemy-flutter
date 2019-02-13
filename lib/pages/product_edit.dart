0fca35c327a927a1b7630b62cc2dfaed516bd946
Davidson Silva <davidson.f.silva@gmaill.com>
2019-02-13 15:26:25 -0200

    Seção:9 - Working with Forms

---
+++ lib/pages/product_edit.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import '../widgets/helpers/ensure-visible.dart';
+
+class ProductEditPage extends StatefulWidget {
+  final Function addProduct;
+  final Function updateProduct;
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductEditPage(
+      {this.addProduct, this.updateProduct, this.product, this.productIndex});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductEditPageState();
+  }
+}
+
+class _ProductEditPageState extends State<ProductEditPage> {
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'description': Null,
+    'price': null,
+    'image': 'assets/food.jpg'
+  };
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  final FocusNode _titleFocusNode = new FocusNode();
+  final FocusNode _descriptionFocusNode = new FocusNode();
+  final FocusNode _priceFocusNode = new FocusNode();
+
+  Widget _buildTitleTextField() {
+    return EnsureVisibleWhenFocused(
+      focusNode: _titleFocusNode,
+      child: TextFormField(
+        focusNode: _titleFocusNode,
+        decoration: InputDecoration(labelText: 'Product Title'),
+        initialValue: widget.product == null ? '' : widget.product['title'],
+        validator: (String value) {
+          if (value.isEmpty || value.length < 5) {
+            return 'Title is required and should be 5+ characters long.';
+          }
+        },
+        onSaved: (String value) {
+          _formData['title'] = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return EnsureVisibleWhenFocused(
+      focusNode: _descriptionFocusNode,
+      child: TextFormField(
+        focusNode: _descriptionFocusNode,
+        decoration: InputDecoration(labelText: 'Product Description'),
+        maxLines: 4,
+        initialValue:
+            widget.product == null ? '' : widget.product['description'],
+        validator: (String value) {
+          if (value.isEmpty || value.length < 10) {
+            return 'Description is required and should be 10+ characters long.';
+          }
+        },
+        onSaved: (String value) {
+          _formData['description'] = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return EnsureVisibleWhenFocused(
+      focusNode: _priceFocusNode,
+      child: TextFormField(
+        focusNode: _priceFocusNode,
+        decoration: InputDecoration(labelText: 'Product Price'),
+        keyboardType: TextInputType.number,
+        initialValue:
+            widget.product == null ? '' : widget.product['price'].toString(),
+        validator: (String value) {
+          if (value.isEmpty ||
+              !RegExp(r'^(?:[1-9]\d*|0)?(?:[.,]\d+)?$').hasMatch(value)) {
+            return 'Price is required and should be a number.';
+          }
+        },
+        onSaved: (String value) {
+          _formData['price'] = double.parse(value);
+        },
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    if (widget.product == null) {
+      widget.addProduct(_formData);
+    } else {
+      widget.updateProduct(widget.productIndex, _formData);
+    }
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double devicewidth = MediaQuery.of(context).size.width;
+    final double targetWidth = devicewidth > 768.0 ? 500.0 : devicewidth * 0.95;
+    final double paddingWidth = devicewidth - targetWidth;
+    final Widget pageContent = GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: paddingWidth / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text("Save"),
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+
+    return widget.product == null
+        ? pageContent
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Edit Product'),
+            ),
+            body: pageContent,
+          );
+  }
+}
