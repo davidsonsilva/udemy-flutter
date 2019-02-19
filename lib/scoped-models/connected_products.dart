270a8c77a5360e28372ec99cef7face8b8029379
Davidson Silva <davidson.f.silva@gmaill.com>
2019-02-19 15:55:33 -0300

    Unselecting the Product after Editing

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -86,7 +86,9 @@ mixin ProductsModel on ConnectedProductsModel {
 
   void selectProduct(int index) {
     _selProductIndex = index;
-    notifyListeners();
+    if (index != null) {
+      notifyListeners();
+    }
   }
 
   void toggleDisplayMode() {
